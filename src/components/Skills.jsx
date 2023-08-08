3a9c4c9456180f0c82ab17e632275c23aee13ae4
Aadil <90678881+Aadilkhan12@users.noreply.github.com>
2023-08-08 11:26:14 +0530

    Update Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -10,9 +10,9 @@ import Div from "./Div";
 
 // IMAGES
 import sk1 from "../assets/sk-1.png";
-import sk2 from "../assets/sk-2.png";
-import sk3 from "../assets/sk-3.png";
-import sk4 from "../assets/sk-4.png";
+import sk2 from "../assets/python.png";
+import sk3 from "../assets/chakraui.png";
+import sk4 from "../assets/tailwind.png";
 import sk5 from "../assets/sk-5.png";
 import sk6 from "../assets/sk-6.png";
 import sk7 from "../assets/sk-7.png";
@@ -20,7 +20,9 @@ import sk8 from "../assets/sk-8.png";
 import sk9 from "../assets/sk-9.png";
 import sk10 from "../assets/sk-10.png";
 import sk11 from "../assets/sk-11.png";
+import sk12 from "../assets/nextjs.png";
 import pattern from "../assets/heading-pattern.png";
+
 import pe1 from "../assets/sec-3-p-e-1.png";
 import pe2 from "../assets/sec-3-p-e-2.png";
 import pe3 from "../assets/sec-3-p-e-3.png";
@@ -45,17 +47,17 @@ const Skills = () => {
             <Wrapper>
                 {/* SKILL ICONS START */}
                 <Div className="grid grid-cols-4 gap-3 md:grid-cols-11 relative">
-                    <SkillIcon path={sk1} />
-                    <SkillIcon path={sk2} />
-                    <SkillIcon path={sk3} />
-                    <SkillIcon path={sk4} />
-                    <SkillIcon path={sk5} />
-                    <SkillIcon path={sk6} />
-                    <SkillIcon path={sk7} />
                     <SkillIcon path={sk8} />
                     <SkillIcon path={sk9} />
                     <SkillIcon path={sk10} />
                     <SkillIcon path={sk11} />
+                    <SkillIcon path={sk5} />
+                    <SkillIcon path={sk1} />
+                    <SkillIcon path={sk12} />
+                    <SkillIcon path={sk4} />
+                    <SkillIcon path={sk3} />
+                    <SkillIcon path={sk2} />
+                    
                 </Div>
                 {/* SKILL ICONS END */}
 
